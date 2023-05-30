cb36a20a5e0dc1f18efe40c6293a37495592bf98
Korayckr <koraycakir1@hotmail.com>
2023-05-30 14:04:09 +0300

    Add project files.

---
+++ ConsoleAppPolymorphism/EnglishPeople.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPolymorphism
+{
+    class EnglishPeople : insan
+    {
+        public override void change()
+        {
+            Console.Title = "Bu bir örnektir.";
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello MR. and MSS. ....");
+        }
+
+
+
+
+    }
+}
---
+++ ConsoleAppPolymorphism/insan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPolymorphism
+{
+
+
+
+
+    // Polymorphismde amaç kalıtım yapılan sınıftan bazı değerleri alıp kullanabilmek
+    // Bunu yaparken de override yani ezme işlemi uygulanır.
+    // Aşağıda yapılmakta olan örnekte insan isimli bir sınıf olacak ve
+    // bu sınıfın içerisinden tanımlı sanal bir metodumun yardımıyla arka plan rengi değiştirilmiştir.
+    class insan
+    {
+
+
+        public virtual void change()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Title = "BU BİR ÖRNEKTİR";
+            Console.WriteLine("");
+            Console.WriteLine("BU EZİLMEDEN ÖNCEKİ TEXT");
+
+
+        }
+
+
+    }
+}
